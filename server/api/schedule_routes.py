4a9b55737a0efb3c7ad910b5367cf4a3fe81a000
Arman Chinai <chinaiarman@gmail.com>
2024-10-01 19:05:28 -0700

    Create schedule routes

--- server/api/schedule_routes.py
+++ server/api/schedule_routes.py
@@ -13,3 +13,19 @@ schedule_bp = Blueprint('schedule_bp', __name__)
 @schedule_bp.route('/schedule/', methods=['GET'])
 def get_schedules():
     return jsonify({"message": "schedule root endpoint"})
+
+@schedule_bp.route('/schedule/<int:id>/', methods=['GET'])
+def get_schedule_by_id(id):
+    return jsonify({"message": f"schedule {id} endpoint"})
+
+@schedule_bp.route('/schedule/', methods=['POST'])
+def create_schedule():
+    return jsonify({"message": "schedule create endpoint"})
+
+@schedule_bp.route('/schedule/<int:id>/', methods=['PUT'])
+def update_schedule(id):
+    return jsonify({"message": f"schedule {id} update endpoint"})
+
+@schedule_bp.route('/schedule/<int:id>/', methods=['DELETE'])
+def delete_schedule(id):
+    return jsonify({"message": f"schedule {id} delete endpoint"})
