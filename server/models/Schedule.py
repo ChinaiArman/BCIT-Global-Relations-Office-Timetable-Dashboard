29702da109d12d83b2050b5632bfd4558f55bae7
Arman Chinai <113311772+ChinaiArman@users.noreply.github.com>
2024-10-04 11:45:56 -0700

    Update Schedule.py

--- server/models/Schedule.py
+++ server/models/Schedule.py
@@ -8,9 +8,18 @@
 class Schedule:
     """
     """
-    def __init__(self):
+    def __init__(self, courses: list, student_id: str):
         """
         """
-        pass
+        self.courses = courses
+        self.student_id = student_id
 
-    
+    def get_courses(self):
+        """
+        """
+        return self.courses
+
+    def get_student_id(self):
+        """
+        """
+        return self.student_id
