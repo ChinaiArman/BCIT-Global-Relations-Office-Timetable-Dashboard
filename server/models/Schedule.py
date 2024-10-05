a6da8f43783f15be160b8c5fdeaba9d89d15443a
Arman Chinai <chinaiarman@gmail.com>
2024-10-05 15:07:08 -0700

    Init schedule back

---
+++ server/models/Schedule.py
@@ -0,0 +1,15 @@
+"""
+"""
+
+# IMPORTS
+
+
+# Schedule DATA CLASS
+class Schedule:
+    """
+    """
+    def __init__(self, student, courses=[]):
+        """
+        """
+        self.student = student
+        self.courses = courses
