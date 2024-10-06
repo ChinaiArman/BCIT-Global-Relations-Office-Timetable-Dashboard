436edd29cafff2d81b06736aecc440cd059d8d27
cz1501 <122151441+cz1501@users.noreply.github.com>
2024-10-06 15:20:49 -0700

    Add implementation to student routes

--- server/api/student_routes.py
+++ server/api/student_routes.py
@@ -6,57 +6,77 @@ from flask import Blueprint, jsonify, request, current_app
 
 
 # INSTANTIATE SERVICES
-
+from services.Database import Database
+from models.Student import Student
 
 # DEFINE BLUEPRINT
-student_bp = Blueprint('student_bp', __name__)
+student_bp = Blueprint("student_bp", __name__)
 
 
 # ROUTES
-@student_bp.route('/students/<int:id>/', methods=['GET'])
+@student_bp.route("/students/<int:id>/", methods=["GET"])
 def get_student_by_id(id):
-    """
-    """
-    return jsonify({"message": f"student {id} endpoint"})
+    db = current_app.config["database"]
+    response = db.get_student_by_id(id)
+    return (
+        jsonify({"message": response["message"], "data": response["data"]}),
+        response["status"],
+    )
 
-@student_bp.route('/students/', methods=['POST'])
+
+@student_bp.route("/students/", methods=["POST"])
 def create_student():
-    """
-    """
-    return jsonify({"message": "student create endpoint"})
+    """ """
+    data = request.get_json()
+    # Need to match the keys of the data with the Student parameters
+    student = Student(**data)
+    db = current_app.config["database"]
+    response = db.create_student(student)
+    return jsonify({"message": response["message"]}), response["status"]
 
-@student_bp.route('/students/<int:id>/', methods=['PUT'])
+
+@student_bp.route("/students/<int:id>/", methods=["PUT"])
 def update_student(id):
-    """
-    """
-    return jsonify({"message": f"student {id} update endpoint"})
+    """ """
+    db = current_app.config["database"]
+    data = request.get_json()
+    response = db.update_student(id, data)
+    return jsonify({"message": response["message"]}), response["status"]
 
-@student_bp.route('/students/<int:id>/', methods=['DELETE'])
+
+@student_bp.route("/students/<int:id>/", methods=["DELETE"])
 def delete_student(id):
-    """
-    """
-    return jsonify({"message": f"student {id} delete endpoint"})
+    """ """
+    db = current_app.config["database"]
+    response = db.delete_student(id)
+    return jsonify({"message": response["message"]}), response["status"]
 
-@student_bp.route('/students/import', methods=['PUT'])
+
+@student_bp.route("/students/import", methods=["PUT"])
 def upload_students():
-    """
-    """
+    """ """
     return jsonify({"message": "student import endpoint"})
 
-@student_bp.route('/students/export', methods=['GET'])
+
+@student_bp.route("/students/export", methods=["GET"])
 def download_students():
-    """
-    """
+    """ """
     return jsonify({"message": "student export endpoint"})
 
-@student_bp.route('/students/<int:id>/courses/', methods=['GET'])
-def get_student_courses(id):
-    """
-    """
-    return jsonify({"message": f"student {id} courses endpoint"})
 
-@student_bp.route('/students/download_template', methods=['GET'])
+@student_bp.route("/students/<int:id>/courses/", methods=["GET"])
+def get_student_courses(id):
+    """ """
+    db = current_app.config["database"]
+    response = db.get_student_courses(id)
+    return (
+        jsonify({"message": response["message"], "data": response["data"]}),
+        response["status"],
+    )
+
+
+@student_bp.route("/students/download_template", methods=["GET"])
 def download_template():
-    """
-    """
+    """ """
+
     return jsonify({"message": "student download template endpoint"})
