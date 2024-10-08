3abdf91326e8dc45ef0a556fbb702d3a1c73b3ab
Arman Chinai <113311772+ChinaiArman@users.noreply.github.com>
2024-10-07 18:44:16 -0700

    Add strict limit to num preferences

--- server/models/Student.py
+++ server/models/Student.py
@@ -14,7 +14,7 @@ class Student(db.Model):
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
     term_code = db.Column(db.Integer, nullable=False)
-    preferences = db.Column(db.String(256), default='')
+    preferences = db.Column(db.String(71), default='')
 
     # Use a string reference for the Course class
     courses = db.relationship('Course', secondary=enrollments, back_populates='students')
