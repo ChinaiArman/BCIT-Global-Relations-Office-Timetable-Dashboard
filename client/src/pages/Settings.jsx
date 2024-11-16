8a8ed459823301da974c021f886bb518d34447be
cz1501 <122151441+cz1501@users.noreply.github.com>
2024-11-16 08:38:42 -0800

    Add Settings Page

--- client/src/pages/Settings.jsx
+++ client/src/pages/Settings.jsx
@@ -1,9 +1,16 @@
+import React, { useState } from "react";
+import Header from "../components/Header";
+import UserInfoCard from "../components/UserInfoCard";
+
 const Settings = () => {
     return (
-        <div>
-            <h2>Welcome to the Settings Page</h2>
-        </div>
-    );
+		<div className='flex-1 overflow-auto relative z-10 bg-gray-900'>
+			<Header title='Settings' />
+			<main className='max-w-4xl mx-auto py-6 px-4 lg:px-8'>
+				<UserInfoCard />
+			</main>
+		</div>
+	);
 };
 
 export default Settings;
